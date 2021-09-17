92b5179b474720f5923d64f81b2ea832540791ad
NEAThorium-Team <90842673+NEAThorium-Team@users.noreply.github.com>
2021-09-17 04:43:02 +0200

    Updated reference to the specific hash for JS Initializer constants. (#2)

--- src/main/java/com/neathorium/thorium/framework/selenium/constants/scripts/injectable/JSInitializerConstants.java
+++ src/main/java/com/neathorium/thorium/framework/selenium/constants/scripts/injectable/JSInitializerConstants.java
@@ -1,7 +1,7 @@
 package com.neathorium.thorium.framework.selenium.constants.scripts.injectable;
 
 public abstract class JSInitializerConstants {
-    private static final String BASE = "https://cdn.jsdelivr.net/gh/neathorium/thorium-selenium@e767aa3b896741077621fd97e4f6d0c428d9f5ff/js-utils/";
+    private static final String BASE = "https://cdn.jsdelivr.net/gh/Neathorium/thorium-selenium@2dc4336c55732c8adf43821ca8c8694b09269d50/js-utils/";
     public static final String STRINGULARITY_TYPE_UTILS = BASE + "stringularity-type-utils-nodocs-rollup.js";
     public static final String THORIUM_UTILITIES = BASE + "thoriumutilities.js";
 
